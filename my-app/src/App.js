df4bb6ea7ae863ca8a7fed8867d6a1943220927c
Vijender Kumar Panda <kumar.vijender@bcg.com>
2022-04-19 13:48:42 +0530

    cvbxcb

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Vijender.
+          Edit <code>src/App.js</code> and save to reload Vijender panda.
         </p>
         <a
           className="App-link"
